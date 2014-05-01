62328fa0e4dce181f09e948b12bbedd75bc0efa9
Zach Latta <zach@zachlatta.com>
2014-04-30 20:04:53 -0700

    Refactor mail delivery logic to separate file.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"io"
-	stdMail "net/mail"
 	"os"
 	"text/template"
 
@@ -144,40 +143,4 @@ func printMissingFlags(w io.Writer, missingFlags []*flag.Flag) {
 func missingFlags(missingFlags []*flag.Flag) {
 	printMissingFlags(os.Stderr, missingFlags)
 	os.Exit(2)
-}
-
-func sendMail(recipient Recipient, emailField string, mailer *mail.Mailer,
-	success chan Recipient, fail chan error) {
-
-	parsedSender, err := stdMail.ParseAddress(sender)
-	if err != nil {
-		fail <- err
-		return
-	}
-
-	parsedTo, err := stdMail.ParseAddress(recipient[emailField])
-	if err != nil {
-		fail <- err
-		return
-	}
-
-	message, err := mail.NewMessage(
-		parsedSender,
-		parsedTo,
-		subject,
-		textTemplatePath,
-		htmlTemplatePath,
-		recipient,
-	)
-	if err != nil {
-		fail <- err
-		return
-	}
-
-	if err := mailer.Send(message); err != nil {
-		fail <- err
-		return
-	}
-
-	success <- recipient
 }
