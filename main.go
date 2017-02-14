71ef54f950c697ed33d65154f4d06089560dc74f
Carlos Moreno <cmoreno@clmoreno.com>
2017-02-13 23:44:06 -0600

    Added support for smtp servers with no authentication. Some stmp servers have no authentication, removing user and password from required flags adds support for those smtp servers without authentication.

--- main.go
+++ main.go
@@ -43,8 +43,14 @@ func main() {
 	flag.StringVar(&attach, "attach", "", "attach a list of comma separated files")
 	flag.IntVar(&workerCount, "c", 8, "number of concurrent requests to have")
 
-	requiredFlagNames := []string{"text", "csv", "server", "port", "user",
-		"password", "sender", "subject"}
+	requiredFlagNames := []string{
+		"text",
+		"csv",
+		"server",
+		"port",
+		"sender",
+		"subject",
+	}
 	flag.VisitAll(func(f *flag.Flag) {
 		flags = append(flags, f)
 
